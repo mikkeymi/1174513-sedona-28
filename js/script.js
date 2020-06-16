612fdec8621816dd144b2e279d38564e0fd9c17c
Anastasia <yfcnz.1998.1998@mail.ru>
2020-06-16 14:02:38 +0400

    продолжаем оформление декоративных элементов

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-let closeForm = document.querySelector(".class")
+
+
+var showForm = document.querySelector(".search_hotels");
+var modalForm = document.querySelector(".form_body");
+
+showForm.addEventListener("click", function () {
+
+
+	console.log(modalForm.classList);
+	modalForm.classList.toggle("modal-hide");
+	console.log(modalForm.classList);
+});
+
