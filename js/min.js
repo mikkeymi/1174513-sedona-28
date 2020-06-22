cfc0f7c2da3e95038cdb34722e9a7ba5b708a637
Anastasia <yfcnz.1998.1998@mail.ru>
2020-06-22 19:01:14 +0400

    правки2

---
+++ js/min.js
@@ -0,0 +1,1 @@
+var showForm=document.querySelector(".search_hotels"),modalForm=document.querySelector(".form_body");showForm.addEventListener("click",function(){console.log(modalForm.classList),modalForm.classList.toggle("modal-hide"),console.log(modalForm.classList)});
