452dbdf299b8577a19ce11d3126a6e7a743edfd8
Anastasia <yfcnz.1998.1998@mail.ru>
2020-06-25 11:14:48 +0400

    седона готова

--- js/min.js
+++
@@ -1,1 +0,0 @@
-var showForm=document.querySelector(".search_hotels"),modalForm=document.querySelector(".form_body");showForm.addEventListener("click",function(){console.log(modalForm.classList),modalForm.classList.toggle("modal-hide"),console.log(modalForm.classList)});
--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-
-
 var showForm = document.querySelector(".search_hotels");
 var modalForm = document.querySelector(".form_body");
+var formLogin = document.querySelector(".form_search");
+var arrivalDate = document.querySelector("#arrival_date");
+var departuresDate = document.querySelector(".date_departures");
+var quantityAdults = document.querySelector(".adults_quantity");
 
-showForm.addEventListener("click", function () {
-
-
-	console.log(modalForm.classList);
-	modalForm.classList.toggle("modal-hide");
-	console.log(modalForm.classList);
+document.addEventListener("DOMContentLoaded", function () {
+    modalForm.classList.add("modal_hide");
 });
 
+showForm.addEventListener("click", function () {
+	modalForm.classList.toggle("modal_hide");
+	modalForm.classList.remove("form_error");
+});
+
+formLogin.addEventListener("submit", function (evt) {
+	if (!arrivalDate.value || !departuresDate.value || 
+		!quantityAdults.value) {  
+		evt.preventDefault();
+		modalForm.classList.remove("form_error");
+		modalForm.offsetWidth = modalForm.offsetWidth;
+		modalForm.classList.add("form_error");
+	}  
+});
